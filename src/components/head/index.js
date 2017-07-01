2a6da48668682533925a877b34fcdc1ec0551474
Wuyialex <cci45@naver.com>
2017-07-01 17:50:23 +0900

    enableFullScreen

--- src/components/head/index.js
+++ src/components/head/index.js
@@ -1,15 +1,46 @@
 // @ flow
 import React from 'react';
+import screenfull from 'screenfull';
+import { connect } from 'react-redux';
+import { fullScreenChange } from '../../actions/AppActions';
 import parentCss from '../app/app.css';
 import css from './head.css';
 import icFullScreenIcon from '../../../static/icons/ic_fullScreen.svg';
 
-const Head = () => (
-	<div className={parentCss.head}>
-		<div className={css.container}>
-			<img className={css.icon} src={icFullScreenIcon} />
-		</div>
-	</div>
-);
+const mapToDispatch = dispatch => ({
+	changeFullScreenApp(argu) {
+		dispatch(fullScreenChange(argu));
+	},
+});
 
-export default Head;
+const fullScreen = () => {
+	if (screenfull.enabled) {
+		screenfull.toggle();
+	} else {
+      // Ignore or do something else
+	}
+};
+
+const Head = ({ changeFullScreenApp }) => {
+	screenfull.onchange(() => {
+		if (screenfull.isFullscreen) {
+			changeFullScreenApp(true);
+		} else {
+			changeFullScreenApp(false);
+		}
+	});
+
+	return (
+		<div className={parentCss.head}>
+			<div className={css.container}>
+				<img
+					className={css.icon}
+					src={icFullScreenIcon}
+					onClick={fullScreen}
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default connect(undefined, mapToDispatch)(Head);
