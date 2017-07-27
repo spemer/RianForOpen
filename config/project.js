409b5d18f8312f05bd21084bd1a2c148ceea0641
Wuyialex <cci45@naver.com>
2017-07-27 16:59:00 +0900

    fixIP

--- config/project.js
+++ config/project.js
@@ -2,6 +2,7 @@
 const IsAWS = process.env.AWS_IP !== undefined;
 export const PORT = process.env.PORT || 4000;
 export const IP_ENV = IsAWS ? process.env.AWS_IP : 'localhost';
+console.log('ISAWS', process.env.AWS_IP, IP_ENV);
 export const APOLLO = {
 	uri: `http://${IP_ENV}:${PORT}/api/graphql`,
 };
