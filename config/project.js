362004db04a9b2658185dc4b8f085ad575bcdaaa
Wuyialex <cci45@naver.com>
2017-07-27 17:19:16 +0900

    fixAWS

--- config/project.js
+++ config/project.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 const IsAWS = process.env.AWS_IP !== undefined;
 export const PORT = process.env.PORT || 4000;
-export const IP_ENV = IsAWS ? process.env.AWS_IP : 'localhost';
+export const IP_ENV = IsAWS ? 'riannote.com' : 'localhost';
 console.log('ISAWS', process.env.AWS_IP, IP_ENV);
 export const APOLLO = {
 	uri: `http://${IP_ENV}:${PORT}/api/graphql`,
