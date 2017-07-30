a3df7813260713768713633754c7a81109be5bbc
Wuyialex <cci45@naver.com>
2017-07-31 03:28:32 +0900

    fixBug

--- src/components/login/isMobile/index.js
+++ src/components/login/isMobile/index.js
@@ -5,11 +5,12 @@ import css from './isMobile.css';
 const isMobile = () => (
 	<div id={css.mainLogin}>
 		<div className={css.box}>
-			<div className={css.subTitle}>
+			<div className={css.title}>
                 죄송합니다.
                 <br />
                 현재 모바일에서는 리안을 사용하실 수 없습니다.
-                <br />
+			</div>
+            <div className={css.subTitle}>
                 빠른 시일내에 모바일 리안으로 찾아뵐 수 있도록 노력하겠습니다.
                 <br />
 			</div>
