fe5b42001a19cc2d911be521cb29e7d8a6b55092
Wuyialex <cci45@naver.com>
2017-07-31 23:12:54 +0900

    fixImageUpload

--- src/components/note/noteRianEditor/editorConfig.js
+++ src/components/note/noteRianEditor/editorConfig.js
@@ -1,5 +1,4 @@
-// const moment = require('moment');
-// const crypto = require('crypto');
+
 
 const toolbarButton = [
 	'paragraphFormat',
@@ -17,35 +16,6 @@ const toolbarButton = [
 	'html',
 ]; //
 
-// const s3Config = {
-// 	bucket: 'elasticbeanstalk-us-east-2-515925319740',
-// 	region: 's3',
-// 	acl: 'public-read',
-// 	keyStart: 'uploads/',
-// 	params: {
-// 		acl: 'public-read',
-// 		AWSAccessKeyId: 'AKIAIAYA55I2O72V4Y5Q',
-// 	},
-// };
-
-// s3Config.params.policy = {
-// 	expiration: moment().add(1, 'days').toISOString(),
-// 	conditions: [
-// 	{ bucket: s3Config.bucket },
-// 	{ acl: s3Config.params.acl },
-// 	{ success_action_status: '201' },
-// 	{ 'x-requested-with': 'xhr' },
-// 	['starts-with', '$key', s3Config.keyStart],
-// 	['starts-with', '$Content-Type', ''],
-// 	],
-// };
-// s3Config.params.policy = new Buffer(JSON.stringify(s3Config.params.policy)).toString('base64');
-
-// const hash = crypto.createHmac('sha1', 'AKIAIAYA55I2O72V4Y5Q');
-// s3Config.params.signature = new Buffer(hash.update(s3Config.params.policy)
-// 	.digest())
-// 	.toString('base64');
-
 const config = {
 	spellcheck: false,
 	placeholderText: '',
@@ -55,7 +25,6 @@ const config = {
 	tabSpaces: 8,
 	language: 'ko',
 	toolbarInline: true,
-	videoInsertButtons: ['videoBack', 'videoByURL'],
 	linkList: [],
 	pluginsEnabled: [
 		'lists',
@@ -70,8 +39,8 @@ const config = {
 	],
 	shortcutsEnabled: ['paragraphFormat', 'undo', 'redo'],
 	pastePlain: true,
-	imageDefaultWidth: 400,
 	imageResize: true,
+	imageInsertButtons: ['imageBack', '|', 'imageByURL'],
 	imageEditButtons: [],
 	toolbarVisibleWithoutSelection: false,
 	colorsDefaultTab: 'background',
@@ -88,7 +57,6 @@ const config = {
 		H2: 'H2',
 		H1: 'H1',
 	},
-  // imageUploadToS3: s3Config,
 };
 
 export default config;
